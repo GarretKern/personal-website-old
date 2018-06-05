f0606eda2d9d3b13c845810de3117e2c14420e5f
Garret Kern <garretkern@gmail.com>
2018-06-05 13:37:12 -0400

    Typed js testing

---
+++ main.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".write").typed({
+      strings: ["Sophmore", "Runner", "Reader", "Brunonian", "Developer^1000"],
+      typeSpeed: 50,
+      backSpeed: 20,
+      loop: true
+  });
+})
