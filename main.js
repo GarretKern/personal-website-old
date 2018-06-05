dc9eb2f960f907c58db2d23143aeb5d4825b959c
Garret Kern <garretkern@gmail.com>
2018-06-05 19:12:41 -0400

    Layout planning

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 $(function(){
-  $(".write").typed({
-      strings: ["Sophmore", "Runner", "Reader", "Brunonian", "Developer^1000"],
+  $(".iama").typed({
+      strings: ["Statistician", "Epidemiologist", "Developer^5000"],
+      typeSpeed: 50,
+      backSpeed: 20,
+      loop: true,
+  });
+})
+
+$(function(){
+  $(".projectsin").typed({
+      strings: ["Python", "Javascript", "Node.js", "Java", "R+ggplot"],
       typeSpeed: 50,
       backSpeed: 20,
       loop: true
   });
 })
+
+function openTab(evt, tabName) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
