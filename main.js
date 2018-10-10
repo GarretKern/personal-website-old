892adeb3a1b675dd28f97fa938774e31d12545c9
unknown <garretkern@gmail.com>
2018-10-09 21:21:01 -0400

    added cs365 project

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ function openTab(evt, tabName) {
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
+        // TODO: class list
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
